ecfb1e9def1ab578cdb89d4a713257e846febf76
yungbali <info.prodbybali@gmail.com>
2024-12-01 05:19:25 +0100

    auth resource updates

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -7,5 +7,12 @@ import { defineAuth } from '@aws-amplify/backend';
 export const auth = defineAuth({
   loginWith: {
     email: true,
+    phone: undefined
   },
+  userAttributes: {
+    email: {
+      required: true,
+      mutable: true
+    }
+  }
 });
